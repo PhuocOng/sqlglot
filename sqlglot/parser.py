c7df26a4e3f38ee75a1c6de2d1f2044adc6b00d5
Trey Spiller <treyspiller@gmail.com>
2026-08-27 16:44:17 -0500

    differentiate grouping sets as group by element

--- sqlglot/parser.py
+++ sqlglot/parser.py
@@ -5578,6 +5578,9 @@ class Parser:
                     )
                 )
             )
+            grouping_sets_not_group_by_element = (
+                bool(elements["expressions"]) and self._prev.token_type != TokenType.COMMA
+            )
 
             before_with_index = self._index
             with_prefix = self._match(TokenType.WITH)
@@ -5587,6 +5590,9 @@ class Parser:
                 elements[key].append(cube_or_rollup)
             elif grouping_sets := self._parse_grouping_sets():
                 elements["grouping_sets"].append(grouping_sets)
+                elements.setdefault(
+                    "grouping_sets_not_group_by_element", grouping_sets_not_group_by_element
+                )
             elif self._match_text_seq("TOTALS"):
                 elements["totals"] = True  # type: ignore
 
